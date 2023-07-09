de7811b71c735eb05d3e3be43b104592312062c1
Mylah Dee <vixeliz@protonmail.com>
2023-07-09 10:48:32 -0500

    Disable log

--- src/rimca/src/main.rs
+++ src/rimca/src/main.rs
@@ -3,59 +3,70 @@ use config::Config;
 
 mod logger;
 
-use structopt::StructOpt;
-use structopt::clap::AppSettings;
 use std::str::FromStr;
+use structopt::clap::AppSettings;
 use structopt::clap::Error;
+use structopt::StructOpt;
 
 pub fn main() {
-    logger::init("debug").map_err(|err| eprintln!("logger failed to initialise: {:?}", err)).unwrap();
+    // logger::init("debug").map_err(|err| eprintln!("logger failed to initialise: {:?}", err)).unwrap();
     let cfg: Config = confy::load("rimca", "config").unwrap();
 
-	match Arguments::from_args().command {
-		Command::Login => rimca::login(&cfg.base_dir).unwrap(),
-        Command::Logout{ username } => rimca::logout(&username, &cfg.base_dir).unwrap(),
-		Command::Delete{ instance } => rimca::delete(&instance, &cfg.base_dir).unwrap(),
-        
+    match Arguments::from_args().command {
+        Command::Login => rimca::login(&cfg.base_dir).unwrap(),
+        Command::Logout { username } => rimca::logout(&username, &cfg.base_dir).unwrap(),
+        Command::Delete { instance } => rimca::delete(&instance, &cfg.base_dir).unwrap(),
+
         Command::Download(dl) => {
-			if let Some(fabric) = dl.fabric {
-                rimca::download(&dl.instance, dl.version, Some(String::from("fabric")), &cfg.base_dir).unwrap()
-			} else {
-                rimca::download(&dl.instance, dl.version, Some(String::from("vanilla")), &cfg.base_dir).unwrap()
-			}
-        },
+            if let Some(fabric) = dl.fabric {
+                rimca::download(
+                    &dl.instance,
+                    dl.version,
+                    Some(String::from("fabric")),
+                    &cfg.base_dir,
+                )
+                .unwrap()
+            } else {
+                rimca::download(
+                    &dl.instance,
+                    dl.version,
+                    Some(String::from("vanilla")),
+                    &cfg.base_dir,
+                )
+                .unwrap()
+            }
+        }
 
         Command::Launch(l) => {
-        	rimca::launch(&l.instance, &l.username, l.game_output, &cfg.base_dir).unwrap()
-        },
+            rimca::launch(&l.instance, &l.username, l.game_output, &cfg.base_dir).unwrap()
+        }
 
         Command::List(list) => {
             if let Some(remote) = list.loader {
                 match remote {
                     Remote::Vanilla => {
-   //       for mut version in rimca::vanilla::api::versions(v.snapshot).unwrap().into_iter().rev() {
-            //  version.release_time.truncate(version.release_time.find("T").unwrap());
-            //  println!("{0: <20} {1: <15} {2: <25}", version.id, version.r#type, version.release_time);
-            // }  
-                    },
+                        //       for mut version in rimca::vanilla::api::versions(v.snapshot).unwrap().into_iter().rev() {
+                        //  version.release_time.truncate(version.release_time.find("T").unwrap());
+                        //  println!("{0: <20} {1: <15} {2: <25}", version.id, version.r#type, version.release_time);
+                        // }
+                    }
 
                     Remote::Fabric => {
-        //  for version in rimca::fabric::api::loaders().unwrap().into_iter().rev() {
-        //      println!("{0: <20} {1: <15}", version.version, version.stable)
-        //  }      
+                        //  for version in rimca::fabric::api::loaders().unwrap().into_iter().rev() {
+                        //      println!("{0: <20} {1: <15}", version.version, version.stable)
+                        //  }
                     }
                 }
             } else {
                 for instance in rimca::list_instances(&cfg.base_dir).unwrap() {
                     println!("{}", instance.as_str());
-                }    
+                }
             }
         }
 
         _ => {}
     }
 }
-
 
 #[derive(StructOpt)]
 pub struct Arguments {
@@ -85,7 +96,6 @@ pub enum Command {
     // #[structopt(no_version, global_settings = &[AppSettings::DisableVersion])]
     // ///Verify integrity of game files of instance
     // Verify { instance: String },
-
     #[structopt(no_version, global_settings = &[AppSettings::DisableVersion])]
     ///Login a user
     Login,
@@ -95,18 +105,27 @@ pub enum Command {
     Logout { username: String },
 }
 
-#[derive(Debug)]
-#[derive(StructOpt)]
+#[derive(Debug, StructOpt)]
 pub struct Download {
     pub instance: String,
     ///Vanilla version [default: latest_version]
     pub version: Option<String>,
-    #[structopt(long, conflicts_with="fabric", value_name="version", require_equals=true)]
+    #[structopt(
+        long,
+        conflicts_with = "fabric",
+        value_name = "version",
+        require_equals = true
+    )]
     ///Include forge [default version: latest_stable_version]
     pub forge: Option<Option<String>>,
-    #[structopt(long, conflicts_with="forge", value_name="version", require_equals=true)]
+    #[structopt(
+        long,
+        conflicts_with = "forge",
+        value_name = "version",
+        require_equals = true
+    )]
     ///Include fabric [default version: latest_stable_version]
-    pub fabric: Option<Option<String>>
+    pub fabric: Option<Option<String>>,
 }
 
 #[derive(StructOpt)]
@@ -120,7 +139,7 @@ pub struct Launch {
 
 #[derive(StructOpt)]
 pub struct List {
-    #[structopt(short = "r", long= "--remote")]
+    #[structopt(short = "r", long = "--remote")]
     ///Print output of game to terminal
     pub loader: Option<Remote>,
     #[structopt(short = "s", long)]
@@ -135,7 +154,7 @@ impl FromStr for Remote {
         match s {
             "vanilla" => Ok(Remote::Vanilla),
             "fabric" => Ok(Remote::Fabric),
-            _ => todo!()
+            _ => todo!(),
         }
     }
 }
